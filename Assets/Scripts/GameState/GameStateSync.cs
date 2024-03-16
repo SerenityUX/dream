74b79fa2d46c7d9ba835361350862476b347ce51
Bill Zhang <billzhangsc@gmail.com>
2024-03-16 01:55:49 -0700

    Whatever, no callbacks

--- Assets/Scripts/GameState/GameStateSync.cs
+++ Assets/Scripts/GameState/GameStateSync.cs
@@ -1,133 +1,66 @@
-//using UnityEngine;
-//using Normal.Realtime;
-//using Normal.Realtime.Serialization;
+using UnityEngine;
+using Normal.Realtime;
+using Normal.Realtime.Serialization;
 
-//public class GameStateSync : RealtimeComponent<GameStateModel>
-//{
+public class GameStateSync : RealtimeComponent<GameStateModel>
+{
+    // Public method to interact with the GameStateModel
+    public string EnterPlayer(string name)
+    {
+        uint playerID = (uint)realtime.clientID;
+        return model.EnterPlayer(playerID, name);
+    }
 
-//    private ScoreDisplay _scoreDisplay;
-//    // private LeaderboardDisplay _leaderboardDisplay;
+    public void ExitPlayer()
+    {
+        uint playerID = (uint)realtime.clientID;
+        model.ExitPlayer(playerID);
+    }
 
-//    protected override void OnRealtimeModelReplaced(GameStateModel previousModel, GameStateModel currentModel)
-//    {
-//        if (previousModel != null)
-//        {
-//            previousModel.playerStatesDidChange -= PlayerStateDidChange;
-//        }
+    public void AddPointsToPlayer(int points)
+    {
+        uint playerID = (uint)realtime.clientID;
+        // Utilize the model's method
+        model.AddPoints(playerID, points);
+    }
 
-//        if (currentModel != null)
-//        {
-//            if (currentModel.isFreshModel)
-//            {
-//                // If Model has no data, we update with local data
+    // Example method to add laps to a player
+    public void AddLapToPlayer()
+    {
+        // Utilize the model's method
+        uint playerID = (uint)realtime.clientID;
+        model.AddLap(playerID);
+    }
 
-//            }
+    public void TakeDamage(int damage)
+    {
+        // Utilize the model's method
+        uint playerID = (uint)realtime.clientID;
+        model.TakeDamage(playerID, damage);
+    }
 
-//            UpdateLeaderboard();
-//            UpdatePersonal();
+    public void GainPowerUp(int powerUpType)
+    {
+        // Utilize the model's method
+        uint playerID = (uint)realtime.clientID;
+        model.AddPowerUp(playerID, powerUpType);
+    }
 
-//            // Initialize the model
-//            currentModel.playerStatesDidChange += PlayerStateDidChange;
-//        }
-//    }
+    public void InflictStatusEffect(int effectType, float duration)
+    {
+        // Utilize the model's method
+        uint playerID = (uint)realtime.clientID;
+        model.AddStatusEffect(playerID, effectType, duration);
+    }
 
-//    // Public method to interact with the GameStateModel
-//    public string EnterPlayer(string name)
-//    {
-//        int playedID = Realtime.clientID();
-//        return model.EnterPlayer(playerID, name);
-//    }
+    public void UpdateStatusEffects()
+    {
+        // Utilize the model's method
+        model.UpdateStatusEffects();
+    }
 
-//    public void ExitPlayer()
-//    {
-//        int playedID = Realtime.clientID();
-//        model.ExitPlayer(playerID);
-//    }
-
-//    public void AddPointsToPlayer(int points)
-//    {
-//        int playedID = Realtime.clientID();
-//        // Utilize the model's method
-//        model.AddPoints(playerID, points);
-//    }
-
-//    // Example method to add laps to a player
-//    public void AddLapToPlayer()
-//    {
-//        int playedID = Realtime.clientID();
-//        // Utilize the model's method
-//        model.AddLap(playerID);
-//    }
-
-//    public void TakeDamage(int damage)
-//    {
-//        int playedID = Realtime.clientID();
-//        // Utilize the model's method
-//        model.TakeDamage(playerID, damage);
-//    }
-
-//    public void GetPowerUp(int powerUpType)
-//    {
-//        int playedID = Realtime.clientID();
-//        // Check if the user has a powerup
-//        if (model.playerStates[model.GetPlayerIndex(playerID)].powerUpType == 0)
-//        {
-//            // Utilize the model's method
-//            model.AddPowerUp(playerID, powerUpType);
-//        }
-//    }
-//    public void InflictStatusEffect(int effectType, float duration)
-//    {
-//        int playedID = Realtime.clientID();
-//        // Utilize the model's method
-//        model.AddStatusEffect(playerID, effectType, duration);
-//    }
-
-//    private void PlayerStateDidChange(PlayerStateModel model, RealtimeArray<PlayerStateModel> playerStates)
-//    {
-//        // Do something with the player state, eg render leaderboard
-//        UpdateLeaderboard();
-//        UpdatePersonal();
-//    }
-
-//    private void UpdateLeaderboard()
-//    {
-//        // Update the canvas
-//    }
-
-//    private void UpdatePersonal()
-//    {
-//        _scoreDisplay.setScore(model.GetPlayerState(Realtime.clientID()).points);
-//    }
-
-//    public RealtimeArray<PlayerStateModel> GetAllPlayerStates()
-//    {
-//        return model.getAllPlayerStates();
-//    }
-
-//    public RealtimeArray<IntModel> GetAllPlayerIDs()
-//    {
-//        return model.getAllPlayerIDs();
-//    }
-
-//    public PlayerStateModel GetPlayerState(int playerID)
-//    {
-//        int index = model.GetPlayerIndex(playerID); // Assuming you implement this method in your model
-//        if (index != -1)
-//        {
-//            return model.playerStates[index];
-//        }
-//        else
-//        {
-//            Debug.LogError("Player not found.");
-//            return null;
-//        }
-//    }
-
-
-//    public void UpdateStatusEffects()
-//    {
-//        // Utilize the model's method
-//        model.UpdateStatusEffects();
-//    }
-//}
+    public RealtimeDictionary<PlayerStateModel> GetAllPlayerStates()
+    {
+        return model.playerstates;
+    }
+}
