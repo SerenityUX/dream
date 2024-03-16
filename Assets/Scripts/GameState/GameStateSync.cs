4fdebb3893cc664da35eb53cae00f93c4546e681
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2024-03-15 23:58:12 -0700

    ok

--- Assets/Scripts/GameState/GameStateSync.cs
+++ Assets/Scripts/GameState/GameStateSync.cs
@@ -1,133 +1,133 @@
-using UnityEngine;
-using Normal.Realtime;
-using Normal.Realtime.Serialization;
+//using UnityEngine;
+//using Normal.Realtime;
+//using Normal.Realtime.Serialization;
 
-public class GameStateSync : RealtimeComponent<GameStateModel>
-{
+//public class GameStateSync : RealtimeComponent<GameStateModel>
+//{
 
-    private ScoreDisplay _scoreDisplay;
-    // private LeaderboardDisplay _leaderboardDisplay;
+//    private ScoreDisplay _scoreDisplay;
+//    // private LeaderboardDisplay _leaderboardDisplay;
 
-    protected override void OnRealtimeModelReplaced(GameStateModel previousModel, GameStateModel currentModel)
-    {
-        if (previousModel != null)
-        {
-            previousModel.playerStatesDidChange -= PlayerStateDidChange;
-        }
+//    protected override void OnRealtimeModelReplaced(GameStateModel previousModel, GameStateModel currentModel)
+//    {
+//        if (previousModel != null)
+//        {
+//            previousModel.playerStatesDidChange -= PlayerStateDidChange;
+//        }
 
-        if (currentModel != null)
-        {
-            if (currentModel.isFreshModel)
-            {
-                // If Model has no data, we update with local data
+//        if (currentModel != null)
+//        {
+//            if (currentModel.isFreshModel)
+//            {
+//                // If Model has no data, we update with local data
 
-            }
+//            }
 
-            UpdateLeaderboard();
-            UpdatePersonal();
+//            UpdateLeaderboard();
+//            UpdatePersonal();
 
-            // Initialize the model
-            currentModel.playerStatesDidChange += PlayerStateDidChange;
-        }
-    }
+//            // Initialize the model
+//            currentModel.playerStatesDidChange += PlayerStateDidChange;
+//        }
+//    }
 
-    // Public method to interact with the GameStateModel
-    public string EnterPlayer(string name)
-    {
-        int playedID = Realtime.clientID();
-        return model.EnterPlayer(playerID, name);
-    }
+//    // Public method to interact with the GameStateModel
+//    public string EnterPlayer(string name)
+//    {
+//        int playedID = Realtime.clientID();
+//        return model.EnterPlayer(playerID, name);
+//    }
 
-    public void ExitPlayer()
-    {
-        int playedID = Realtime.clientID();
-        model.ExitPlayer(playerID);
-    }
+//    public void ExitPlayer()
+//    {
+//        int playedID = Realtime.clientID();
+//        model.ExitPlayer(playerID);
+//    }
 
-    public void AddPointsToPlayer(int points)
-    {
-        int playedID = Realtime.clientID();
-        // Utilize the model's method
-        model.AddPoints(playerID, points);
-    }
+//    public void AddPointsToPlayer(int points)
+//    {
+//        int playedID = Realtime.clientID();
+//        // Utilize the model's method
+//        model.AddPoints(playerID, points);
+//    }
 
-    // Example method to add laps to a player
-    public void AddLapToPlayer()
-    {
-        int playedID = Realtime.clientID();
-        // Utilize the model's method
-        model.AddLap(playerID);
-    }
+//    // Example method to add laps to a player
+//    public void AddLapToPlayer()
+//    {
+//        int playedID = Realtime.clientID();
+//        // Utilize the model's method
+//        model.AddLap(playerID);
+//    }
 
-    public void TakeDamage(int damage)
-    {
-        int playedID = Realtime.clientID();
-        // Utilize the model's method
-        model.TakeDamage(playerID, damage);
-    }
+//    public void TakeDamage(int damage)
+//    {
+//        int playedID = Realtime.clientID();
+//        // Utilize the model's method
+//        model.TakeDamage(playerID, damage);
+//    }
 
-    public void GetPowerUp(int powerUpType)
-    {
-        int playedID = Realtime.clientID();
-        // Check if the user has a powerup
-        if (model.playerStates[model.GetPlayerIndex(playerID)].powerUpType == 0)
-        {
-            // Utilize the model's method
-            model.AddPowerUp(playerID, powerUpType);
-        }
-    }
-    public void InflictStatusEffect(int effectType, float duration)
-    {
-        int playedID = Realtime.clientID();
-        // Utilize the model's method
-        model.AddStatusEffect(playerID, effectType, duration);
-    }
+//    public void GetPowerUp(int powerUpType)
+//    {
+//        int playedID = Realtime.clientID();
+//        // Check if the user has a powerup
+//        if (model.playerStates[model.GetPlayerIndex(playerID)].powerUpType == 0)
+//        {
+//            // Utilize the model's method
+//            model.AddPowerUp(playerID, powerUpType);
+//        }
+//    }
+//    public void InflictStatusEffect(int effectType, float duration)
+//    {
+//        int playedID = Realtime.clientID();
+//        // Utilize the model's method
+//        model.AddStatusEffect(playerID, effectType, duration);
+//    }
 
-    private void PlayerStateDidChange(PlayerStateModel model, RealtimeArray<PlayerStateModel> playerStates)
-    {
-        // Do something with the player state, eg render leaderboard
-        UpdateLeaderboard();
-        UpdatePersonal();
-    }
+//    private void PlayerStateDidChange(PlayerStateModel model, RealtimeArray<PlayerStateModel> playerStates)
+//    {
+//        // Do something with the player state, eg render leaderboard
+//        UpdateLeaderboard();
+//        UpdatePersonal();
+//    }
 
-    private void UpdateLeaderboard()
-    {
-        // Update the canvas
-    }
+//    private void UpdateLeaderboard()
+//    {
+//        // Update the canvas
+//    }
 
-    private void UpdatePersonal()
-    {
-        _scoreDisplay.setScore(model.GetPlayerState(Realtime.clientID()).points);
-    }
+//    private void UpdatePersonal()
+//    {
+//        _scoreDisplay.setScore(model.GetPlayerState(Realtime.clientID()).points);
+//    }
 
-    public RealtimeArray<PlayerStateModel> GetAllPlayerStates()
-    {
-        return model.getAllPlayerStates();
-    }
+//    public RealtimeArray<PlayerStateModel> GetAllPlayerStates()
+//    {
+//        return model.getAllPlayerStates();
+//    }
 
-    public RealtimeArray<IntModel> GetAllPlayerIDs()
-    {
-        return model.getAllPlayerIDs();
-    }
+//    public RealtimeArray<IntModel> GetAllPlayerIDs()
+//    {
+//        return model.getAllPlayerIDs();
+//    }
 
-    public PlayerStateModel GetPlayerState(int playerID)
-    {
-        int index = model.GetPlayerIndex(playerID); // Assuming you implement this method in your model
-        if (index != -1)
-        {
-            return model.playerStates[index];
-        }
-        else
-        {
-            Debug.LogError("Player not found.");
-            return null;
-        }
-    }
+//    public PlayerStateModel GetPlayerState(int playerID)
+//    {
+//        int index = model.GetPlayerIndex(playerID); // Assuming you implement this method in your model
+//        if (index != -1)
+//        {
+//            return model.playerStates[index];
+//        }
+//        else
+//        {
+//            Debug.LogError("Player not found.");
+//            return null;
+//        }
+//    }
 
 
-    public void UpdateStatusEffects()
-    {
-        // Utilize the model's method
-        model.UpdateStatusEffects();
-    }
-}
+//    public void UpdateStatusEffects()
+//    {
+//        // Utilize the model's method
+//        model.UpdateStatusEffects();
+//    }
+//}
--- Assets/Scripts/GameState/PlayerStateModel.cs
+++ Assets/Scripts/GameState/PlayerStateModel.cs
@@ -1,34 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Normal.Realtime.Serialization; // Ensure you have this using directive for RealtimeModel and RealtimeProperty attributes
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Normal.Realtime.Serialization; // Ensure you have this using directive for RealtimeModel and RealtimeProperty attributes
 
-public enum PowerUpType { GreenCarrot, RedCarrot, CarrotPeels, GoldenCarrot }
-public enum StatusEffectType { Silenced, Invincible, Blind, HalfPoints, DoublePoints }
-
-
-
-[RealtimeModel]
-public partial class PlayerStateModel
-{
-    [RealtimeProperty(1, true, true)]
-    private int _laps;
-
-    [RealtimeProperty(2, true, true)]
-    private int _points;
-
-    [RealtimeProperty(3, true, true)]
-    private int _health;
-
-    [RealtimeProperty(4, true, true)]
-    private int _name;
+//public enum PowerUpType { GreenCarrot, RedCarrot, CarrotPeels, GoldenCarrot }
+//public enum StatusEffectType { Silenced, Invincible, Blind, HalfPoints, DoublePoints }
 
 
-    [RealtimeProperty(5, true, true)]
-    private int _powerUpType;
 
-    [RealtimeProperty(6, true, true)]
-    private int _statusEffectType;
-    [RealtimeProperty(7, true, true)]
-    private float _statusEffectDuration;
-}
+//[RealtimeModel]
+//public partial class PlayerStateModel
+//{
+//    [RealtimeProperty(1, true, true)]
+//    private int _laps;
+
+//    [RealtimeProperty(2, true, true)]
+//    private int _points;
+
+//    [RealtimeProperty(3, true, true)]
+//    private int _health;
+
+//    [RealtimeProperty(4, true, true)]
+//    private int _name;
+
+
+//    [RealtimeProperty(5, true, true)]
+//    private int _powerUpType;
+
+//    [RealtimeProperty(6, true, true)]
+//    private int _statusEffectType;
+//    [RealtimeProperty(7, true, true)]
+//    private float _statusEffectDuration;
+//}
