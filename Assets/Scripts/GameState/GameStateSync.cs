166309e6e8995ab43325ebf936297ab0f32be365
Bill Zhang <billzhangsc@gmail.com>
2024-03-15 17:58:00 -0700

    Fix merge conflicts

--- Assets/Scripts/GameState/GameStateSync.cs
+++ Assets/Scripts/GameState/GameStateSync.cs
@@ -5,50 +5,50 @@ using Normal.Realtime.Serialization;
 public class GameStateSync : RealtimeComponent<GameStateModel>
 {
 
-    //    protected override void OnRealtimeModelReplaced(GameStateModel previousModel, GameStateModel currentModel)
-    //    {
-    //        if (previousModel != null)
-    //        {
-    //            // Here, you could unregister from events on the previous model
-    //            // if you had any events to unregister.
-    //        }
+    protected override void OnRealtimeModelReplaced(GameStateModel previousModel, GameStateModel currentModel)
+    {
+        if (previousModel != null)
+        {
+            // Here, you could unregister from events on the previous model
+            // if you had any events to unregister.
+        }
 
-    //        if (currentModel != null)
-    //        {
-    //            // Here, you would register for events on the current model
-    //            // if you have any events to listen to.
-    //        }
-    //    }
+        if (currentModel != null)
+        {
+            // Here, you would register for events on the current model
+            // if you have any events to listen to.
+        }
+    }
 
-    //    // Public method to interact with the GameStateModel
-    //    public string EnterPlayer(int playerID)
-    //    {
-    //        return model.EnterPlayer(playerID);
-    //    }
+    // Public method to interact with the GameStateModel
+    public string EnterPlayer(int playerID)
+    {
+        return model.EnterPlayer(playerID);
+    }
 
-    //    public void ExitPlayer(int playerID)
-    //    {
-    //        model.ExitPlayer(playerID);
-    //    }
+    public void ExitPlayer(int playerID)
+    {
+        model.ExitPlayer(playerID);
+    }
 
-    //    public void AddPointToPlayer(int playerID)
-    //    {
-    //        // Utilize the model's method
-    //        model.AddPoint(playerID);
-    //    }
+    public void AddPointToPlayer(int playerID)
+    {
+        // Utilize the model's method
+        model.AddPoint(playerID);
+    }
 
-    //    // Example method to add laps to a player
-    //    public void AddLapToPlayer(int playerID)
-    //    {
-    //        // Utilize the model's method
-    //        model.AddLap(playerID);
-    //    }
+    // Example method to add laps to a player
+    public void AddLapToPlayer(int playerID)
+    {
+        // Utilize the model's method
+        model.AddLap(playerID);
+    }
 
-    //    public void DealDamageToPlayer(int playerID, int damage)
-    //    {
-    //        // Utilize the model's method
-    //        model.TakeDamage(playerID, damage);
-    //    }
+    public void DealDamageToPlayer(int playerID, int damage)
+    {
+        // Utilize the model's method
+        model.TakeDamage(playerID, damage);
+    }
 
     public void GivePlayerPowerUp(int playerID, int powerUpType)
     {
@@ -60,40 +60,40 @@ public class GameStateSync : RealtimeComponent<GameStateModel>
         }
     }
 
-    //    public void AddStatusEffectToPlayer(int playerID, int effectType, float duration)
-    //    {
-    //        // Utilize the model's method
-    //        model.AddStatusEffect(playerID, effectType, duration);
-    //    }
+    public void AddStatusEffectToPlayer(int playerID, int effectType, float duration)
+    {
+        // Utilize the model's method
+        model.AddStatusEffect(playerID, effectType, duration);
+    }
 
-    //    public RealtimeArray<PlayerStateModel> GetAllPlayerStates()
-    //    {
-    //        return model.getAllPlayerStates();
-    //    }
+    public RealtimeArray<PlayerStateModel> GetAllPlayerStates()
+    {
+        return model.getAllPlayerStates();
+    }
 
     public RealtimeArray<IntModel> GetAllPlayerIDs()
     {
         return model.getAllPlayerIDs();
     }
 
-//    public PlayerStateModel GetPlayerState(int playerID)
-//    {
-//        int index = model.GetPlayerIndex(playerID); // Assuming you implement this method in your model
-//        if (index != -1)
-//        {
-//            return model.playerStates[index];
-//        }
-//        else
-//        {
-//            Debug.LogError("Player not found.");
-//            return null;
-//        }
-//    }
+    public PlayerStateModel GetPlayerState(int playerID)
+    {
+        int index = model.GetPlayerIndex(playerID); // Assuming you implement this method in your model
+        if (index != -1)
+        {
+            return model.playerStates[index];
+        }
+        else
+        {
+            Debug.LogError("Player not found.");
+            return null;
+        }
+    }
 
 
-//    public void UpdateStatusEffects()
-//    {
-//        // Utilize the model's method
-//        model.UpdateStatusEffects();
-//    }
-//}
+    public void UpdateStatusEffects()
+    {
+        // Utilize the model's method
+        model.UpdateStatusEffects();
+    }
+}
--- Assets/Scripts/GameState/PlayerStateModel.cs
+++ Assets/Scripts/GameState/PlayerStateModel.cs
@@ -1,10 +1,10 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using Normal.Realtime.Serialization; // Ensure you have this using directive for RealtimeModel and RealtimeProperty attributes
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime.Serialization; // Ensure you have this using directive for RealtimeModel and RealtimeProperty attributes
 
-//public enum PowerUpType { GreenShell, RedShell, Banana, Star }
-//public enum StatusEffectType { Silenced, Invincible, Blind, HalfPoints, DoublePoints }
+public enum PowerUpType { GreenShell, RedShell, Banana, Star }
+public enum StatusEffectType { Silenced, Invincible, Blind, HalfPoints, DoublePoints }
 
 
 
@@ -14,15 +14,15 @@ public partial class PlayerStateModel
     [RealtimeProperty(1, true, true)]
     private int _laps;
 
-    //    [RealtimeProperty(2, true, true)]
-    //    private int _points;
+    [RealtimeProperty(2, true, true)]
+    private int _points;
 
-    //    [RealtimeProperty(3, true, true)]
-    //    private int _health;
+    [RealtimeProperty(3, true, true)]
+    private int _health;
 
 
-    //    [RealtimeProperty(5, true, true)]
-    //    private int _powerUpType;
+    [RealtimeProperty(5, true, true)]
+    private int _powerUpType;
 
     [RealtimeProperty(6, true, true)]
     private int _statusEffectType;
