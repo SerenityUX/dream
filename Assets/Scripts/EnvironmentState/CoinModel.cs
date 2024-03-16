1acd3708bb044b7423a81e2cf5b12de1377e724c
Bill Zhang <billzhangsc@gmail.com>
2024-03-16 01:57:30 -0700

    Commenting out not used code

--- Assets/Scripts/EnvironmentState/CoinModel.cs
+++ Assets/Scripts/EnvironmentState/CoinModel.cs
@@ -1,18 +1,18 @@
-using Normal.Realtime.Serialization;
-using System.Collections.Generic;
-using UnityEngine;
+// using Normal.Realtime.Serialization;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-[RealtimeModel]
-public partial class CoinModel
-{
-    [RealtimeProperty(1, true, true)]
-    private Vector3 _position;
+// [RealtimeModel]
+// public partial class CoinModel
+// {
+//     [RealtimeProperty(1, true, true)]
+//     private Vector3 _position;
 
-    [RealtimeProperty(2, true, true)]
-    private string _type;
+//     [RealtimeProperty(2, true, true)]
+//     private string _type;
 
-    [RealtimeProperty(3, true, true)]
-    private bool _active;
+//     [RealtimeProperty(3, true, true)]
+//     private bool _active;
 
-    // Additional properties or methods as needed
-}
+//     // Additional properties or methods as needed
+// }
--- Assets/Scripts/EnvironmentState/EnvironmentStateSync.cs
+++ Assets/Scripts/EnvironmentState/EnvironmentStateSync.cs
@@ -1,94 +1,94 @@
-using UnityEngine;
-using Normal.Realtime;
-using Normal.Realtime.Serialization;
-using System.Collections.Generic;
-using System.IO;
+// using UnityEngine;
+// using Normal.Realtime;
+// using Normal.Realtime.Serialization;
+// using System.Collections.Generic;
+// using System.IO;
 
-public class EnvironmentSync : RealtimeComponent<EnvironmentStateModel>
-{
-    [SerializeField] private string environmentDataPath = "Path/To/Your/JSON.json";
+// public class EnvironmentSync : RealtimeComponent<EnvironmentStateModel>
+// {
+//     [SerializeField] private string environmentDataPath = "Path/To/Your/JSON.json";
 
-    public void LoadJson()
-    {
-        LoadEnvironmentDataFromJSON();
-    }
+//     public void LoadJson()
+//     {
+//         LoadEnvironmentDataFromJSON();
+//     }
 
-    private void LoadEnvironmentDataFromJSON()
-    {
-        string filePath = Path.Combine(Application.dataPath, environmentDataPath);
-        if (File.Exists(filePath))
-        {
-            string json = File.ReadAllText(filePath);
-            // Assuming you have defined a structure that matches your JSON
-            EnvironmentData data = JsonUtility.FromJson<EnvironmentData>(json);
+//     private void LoadEnvironmentDataFromJSON()
+//     {
+//         string filePath = Path.Combine(Application.dataPath, environmentDataPath);
+//         if (File.Exists(filePath))
+//         {
+//             string json = File.ReadAllText(filePath);
+//             // Assuming you have defined a structure that matches your JSON
+//             EnvironmentData data = JsonUtility.FromJson<EnvironmentData>(json);
 
-            foreach (var coinData in data.coins)
-            {
-                Vector3 position = new Vector3(coinData.Location[0], coinData.Location[1], coinData.Location[2]);
-                AddCoin(position, coinData.type);
-            }
+//             foreach (var coinData in data.coins)
+//             {
+//                 Vector3 position = new Vector3(coinData.Location[0], coinData.Location[1], coinData.Location[2]);
+//                 AddCoin(position, coinData.type);
+//             }
 
-            foreach (var powerUpData in data.powerUps)
-            {
-                Vector3 position = new Vector3(powerUpData.Location[0], powerUpData.Location[1], powerUpData.Location[2]);
-                AddPowerUp(position, powerUpData.type);
-            }
-        }
-        else
-        {
-            Debug.LogError("Environment data file not found.");
-        }
-    }
+//             foreach (var powerUpData in data.powerUps)
+//             {
+//                 Vector3 position = new Vector3(powerUpData.Location[0], powerUpData.Location[1], powerUpData.Location[2]);
+//                 AddPowerUp(position, powerUpData.type);
+//             }
+//         }
+//         else
+//         {
+//             Debug.LogError("Environment data file not found.");
+//         }
+//     }
 
-    private void AddCoin(Vector3 position, string type)
-    {
-        model.AddCoin(position, type);
-    }
+//     private void AddCoin(Vector3 position, string type)
+//     {
+//         model.AddCoin(position, type);
+//     }
 
-    private void AddPowerUp(Vector3 position, string type)
-    {
-        model.AddPowerUp(position, type);
-    }
+//     private void AddPowerUp(Vector3 position, string type)
+//     {
+//         model.AddPowerUp(position, type);
+//     }
 
-    public void RemoveCoin(int index)
-    {
-        model.RemoveCoin(index);
-    }
+//     public void RemoveCoin(int index)
+//     {
+//         model.RemoveCoin(index);
+//     }
 
-    public void RemovePowerUp(int index)
-    {
-        model.RemovePowerUp(index);
-    }
+//     public void RemovePowerUp(int index)
+//     {
+//         model.RemovePowerUp(index);
+//     }
 
-    public RealtimeArray<CoinModel> GetCoins()
-    {
-        return model.getCoins();
-    }
+//     public RealtimeArray<CoinModel> GetCoins()
+//     {
+//         return model.getCoins();
+//     }
 
-    public RealtimeArray<PowerUpModel> GetPowerUps()
-    {
-        return model.getPowerUps();
-    }
+//     public RealtimeArray<PowerUpModel> GetPowerUps()
+//     {
+//         return model.getPowerUps();
+//     }
 
-    // Define the structure to match your JSON data
-    [System.Serializable]
-    private class EnvironmentData
-    {
-        public CoinData[] coins;
-        public PowerUpData[] powerUps;
-    }
+//     // Define the structure to match your JSON data
+//     [System.Serializable]
+//     private class EnvironmentData
+//     {
+//         public CoinData[] coins;
+//         public PowerUpData[] powerUps;
+//     }
 
-    [System.Serializable]
-    private class CoinData
-    {
-        public float[] Location;
-        public string type;
-    }
+//     [System.Serializable]
+//     private class CoinData
+//     {
+//         public float[] Location;
+//         public string type;
+//     }
 
-    [System.Serializable]
-    private class PowerUpData
-    {
-        public float[] Location;
-        public string type;
-    }
-}
+//     [System.Serializable]
+//     private class PowerUpData
+//     {
+//         public float[] Location;
+//         public string type;
+//     }
+// }
